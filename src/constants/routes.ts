ee80d979f80b5887cbd2ccad3a7809f55232ac33
Esau Silva <esausilva85@gmail.com>
2026-02-03 20:19:48 -0600

    Update API route for slide retrieval (`API_GET_SLIDES`)

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,6 +1,6 @@
 export const WEB_HOME = '/';
 export const WEB_SETTINGS = '/settings';
 
-export const API_GET_SLIDES = '/photos/relative-urls';
+export const API_GET_SLIDES = '/photos/slides';
 export const API_DOWNLOAD_PHOTOS = '/photos/download';
 export const API_DELETE_PHOTOS = '/photos/bulk-delete';
